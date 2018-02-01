49db9fa7116c7484533f733efe8d2b87626b570c
Avinash Tiwari <qwe123kids@gmail.com>
2018-01-31 20:37:28 -0700

    Microservice sample

---
+++ MicroServiceCompleteSetup/currency-exchange-service/src/main/java/com/avinash/springboot/microservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,22 @@
+package com.avinash.springboot.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+	
+	@Bean
+	public AlwaysSampler deafultSampler()
+	{
+		return new AlwaysSampler();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Micro-service-sample
-Micro service sample
+Micro service sample<br/>
+Follwing things are used <br/>
+Config server <br/>
+Eureka Server <br/>
+Load balaner <br/>
+Ribbon <br/>
+Distributed Api using Zuul <br/>
+Distributed Tracing using Zipkin <br/>
+JPA <br>
+Fegin </br/>
